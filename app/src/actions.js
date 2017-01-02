060fa4e398aa4cf79e9e173f5d51ee710cecb1c4
jiangtyd <jiangtyd@gmail.com>
2017-01-01 22:35:45 -0800

    rm superflous actions

--- app/src/actions.js
+++ app/src/actions.js
@@ -4,9 +4,6 @@ export const START_PAINTING = 'START_PAINTING';
 export const PAINT = 'PAINT';
 export const STOP_PAINTING = 'STOP_PAINTING';
 
-export const PAUSE_PAINTING = 'PAUSE_PAINTING';
-export const RESUME_PAINTING = 'RESUME_PAINTING';
-
 export function startPainting(fillId, cellTypes) {
   return {
     type: START_PAINTING,
@@ -26,17 +23,5 @@ export function paint(gridX, gridY) {
 export function stopPainting() {
   return {
     type: STOP_PAINTING
-  }
-}
-
-export function pausePainting() {
-  return {
-    type: PAUSE_PAINTING
-  }
-}
-
-export function resumePainting() {
-  return {
-    type: RESUME_PAINTING
   }
 }
