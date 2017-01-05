31b6e44b34b5449a1a9a2131d8c55a16f92b7d9c
jiangtyd <jiangtyd@gmail.com>
2017-01-04 23:07:10 -0800

    rm create-react-app css stuff

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import logo from './logo.svg';
 import './App.css';
 import PuzzleContainer from './containers/PuzzleContainer';
 import store from './Store';
@@ -9,13 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React!!!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <div>
           <Provider store={store}>
             <PuzzleContainer />
